2a6bffc21eac14d01f0da4b7dfeabbfba9cb2f9a
francine <francinescosta@gmail.com>
2019-02-18 21:44:38 -0300

    Update server.js

--- dist/server.js
+++ dist/server.js
@@ -3,7 +3,7 @@ var path = require('path')
 var serveStatic = require('serve-static')
 
 var app = express()
-app.use(serveStatic(path.join(__dirname, 'spa-mat')))
+app.use(serveStatic(path.join(__dirname, 'dist/spa-mat')))
 console.log(path.join(__dirname, 'spa-mat'))
 var port = process.env.PORT || 5000
 app.listen(port)
